ed16f4b7cfdba1286e4f73e2d711297c2b7ae625
nicola.cristian <nicola.cristian@TAVE-PC19.OSDBSESTO.local>
2020-11-24 13:39:58 +0100

    creation of method run

---
+++ src/javathread/T1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathread;
+
+/**
+ *
+ * @author nicola.cristian
+ */
+public class T1 extends Thread{
+
+    private String s;
+    
+    public T1(String s) {
+
+        this.s=s;
+        this.start(); //il costruttore viene inizializzato e si autoavvia
+    }
+     
+    //metodo che viene eseguito all'avvio del processo
+         public void run(){
+             
+        for(;;) System.out.println(s); 
+}
+         
+         
+ 
+}
